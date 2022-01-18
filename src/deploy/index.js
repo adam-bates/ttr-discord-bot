ad801d66b03f6bcc06b5b473b731c50c28f1b5a9
Adam Bates <adam@adambates.ca>
2022-01-17 23:25:20 -0500

    Setup initial ping commands for discord bot.

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -1,5 +1,36 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+
+const fs = require("fs");
+const path = require("path");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const COMMANDS_DIR = `${__dirname}/../bot/commands`;
+
 const deploy = () => {
-  console.log("Deploying!");
+  const commands = [];
+  const commandFiles = fs
+    .readdirSync(COMMANDS_DIR)
+    .filter((file) => file.endsWith(".js"));
+
+  commandFiles.forEach((file) => {
+    const command = require(path.join(COMMANDS_DIR, file));
+    commands.push(command.data.toJSON());
+  });
+
+  const rest = new REST({ version: "9" }).setToken(
+    process.env.DISCORD_BOT_TOKEN
+  );
+
+  rest
+    .put(Routes.applicationCommands(process.env.DISCORD_APP_CLIENT_ID), {
+      body: commands,
+    })
+    .then(() =>
+      console.log("Successfully registered application commands: %o", commands)
+    )
+    .catch(console.error);
 };
 
 module.exports = {
--- src/serve/index.js
+++ src/serve/index.js
@@ -1,5 +1,62 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+
+const fs = require("fs");
+const path = require("path");
+const { Client, Collection, Intents } = require("discord.js");
+
+const COMMANDS_DIR = `${__dirname}/../bot/commands`;
+const EVENTS_DIR = `${__dirname}/../bot/events`;
+
+const buildCommands = () => {
+  const commands = [];
+
+  const commandFiles = fs
+    .readdirSync(COMMANDS_DIR)
+    .filter((file) => file.endsWith(".js"));
+
+  commandFiles.forEach((file) => {
+    const command = require(path.join(COMMANDS_DIR, file));
+
+    commands.push([command.data.name, command]);
+  });
+
+  return commands;
+};
+
+const setupEvents = (client) => {
+  const eventFiles = fs
+    .readdirSync(EVENTS_DIR)
+    .filter((file) => file.endsWith(".js"));
+
+  eventFiles.forEach((file) => {
+    const event = require(path.join(EVENTS_DIR, file));
+
+    if (event.once) {
+      client.once(event.name, (...args) => event.execute(client, ...args));
+    } else {
+      client.on(event.name, (...args) => event.execute(client, ...args));
+    }
+  });
+};
+
+const startClient = (client) => {
+  client.login(process.env.DISCORD_BOT_TOKEN);
+};
+
 const serve = () => {
-  console.log("Serving!");
+  const client = new Client({
+    intents: [Intents.FLAGS.GUILDS],
+  });
+
+  client.commands = new Collection();
+
+  const commands = buildCommands();
+  commands.forEach(([name, command]) => client.commands.set(name, command));
+
+  setupEvents(client);
+
+  startClient(client);
 };
 
 module.exports = {
