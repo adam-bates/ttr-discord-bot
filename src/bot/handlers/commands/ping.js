c811ce924fab0f3026987feb1a3d5e719d4bebd1
Adam Bates <adam@adambates.ca>
2022-01-18 22:42:08 -0500

    Cleaned up ping command.

--- src/bot/handlers/commands/ping.js
+++ src/bot/handlers/commands/ping.js
@@ -1,26 +1,11 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-// const { read } = require("../../../services/influxdb");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ping")
+    .setName("tlc-ping")
     .setDescription("Replies with Pong!"),
 
   execute: async (_, interaction) => {
-    // const arr = [];
-
-    // read(influx, {
-    //   next: (row, tableMeta) => {
-    //     arr.push(JSON.stringify(tableMeta.toObject(row), null, 2));
-    //   },
-    //   error: (e) => {
-    //     console.error(e);
-    //     console.log("Finished ERROR");
-    //   },
-    //   complete: () => {
-    //     interaction.reply(arr.join("\n"));
-    //   },
-    // });
     await interaction.reply("Pong!");
   },
 };
--- src/bot/handlers/commands/test.js
+++ src/bot/handlers/commands/test.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+// const { read } = require("../../../services/influxdb");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -28,6 +29,20 @@ module.exports = {
     ),
 
   execute: async (_, interaction) => {
+    // const arr = [];
+
+    // read(influx, {
+    //   next: (row, tableMeta) => {
+    //     arr.push(JSON.stringify(tableMeta.toObject(row), null, 2));
+    //   },
+    //   error: (e) => {
+    //     console.error(e);
+    //     console.log("Finished ERROR");
+    //   },
+    //   complete: () => {
+    //     interaction.reply(arr.join("\n"));
+    //   },
+    // });
     const message = await interaction.reply({
       content: "Hello world!",
       fetchReply: true,
