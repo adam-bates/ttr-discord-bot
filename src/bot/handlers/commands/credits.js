06c87fa9ee4568cede957bb4c19f63fd0d9da8e9
Adam Bates <adam@adambates.ca>
2022-01-20 10:22:12 -0500

    Added more initial unimplemented commands.

---
+++ src/bot/handlers/commands/credits.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("tlc-credits")
+    .setDescription("TODO")
+    .addBooleanOption((option) =>
+      option
+        .setName("public")
+        .setDescription("Make the output of this command public to the server")
+        .setRequired(false)
+    ),
+
+  execute: async (_, interaction) => {
+    const isPublic = interaction.options.getBoolean("public");
+
+    await interaction.reply({ content: "TODO", ephemeral: !isPublic });
+  },
+};
