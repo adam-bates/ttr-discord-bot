60d7cce183ca7051f739b41575ca012755f34781
Adam Bates <adam@adambates.ca>
2022-01-22 10:47:30 -0500

    Forced cronitor when cron is running.

--- src/cron.js
+++ src/cron.js
@@ -1,31 +1,23 @@
 require("dotenv").config();
 
-let cron = require("node-cron");
+const cron = require("node-cron");
+const cronitor = require("cronitor")(process.env.CRONITOR_API_KEY);
 const { fetchData, sendMessages } = require("./bot");
 
-let schedule = (_name, key, callback) => cron.schedule(key, callback);
-
-if (process.env.CRONITOR_API_KEY) {
-  // eslint-disable-next-line global-require
-  const cronitor = require("cronitor")(process.env.CRONITOR_API_KEY);
-  cronitor.wraps(cron);
-  cron = cronitor;
-
-  schedule = (name, key, callback) => cronitor.schedule(name, key, callback);
-}
+cronitor.wraps(cron);
 
 // Every 3 hours
-schedule("Fetch data", "0 */3 * * *", () => fetchData());
+cronitor.schedule("Fetch data", "0 */3 * * *", () => fetchData());
 
 // Every day at midnight
-schedule("Fetch daily data", "0 0 * * *", () =>
+cronitor.schedule("Fetch daily data", "0 0 * * *", () =>
   fetchData({ isDayStart: true })
 );
 
 // Every Monday at midnight
-schedule("Fetch weekly data", "0 0 * * 1", () =>
+cronitor.schedule("Fetch weekly data", "0 0 * * 1", () =>
   fetchData({ isWeekStart: true })
 );
 
 // Every 5 minutes
-cron.schedule("Send messages", "*/5 * * * *", () => sendMessages());
+cronitor.schedule("Send messages", "*/5 * * * *", () => sendMessages());
