e9e50848fac24bd71ff229b3140820ba789f534f
Adam Bates <adam@adambates.ca>
2022-01-22 01:22:04 -0500

    Removed interaction requests from error handling in interactionCreate event.

--- src/bot/handlers/events/interactionCreate.js
+++ src/bot/handlers/events/interactionCreate.js
@@ -15,33 +15,6 @@ module.exports = {
       await execute({ client, ...rest }, interaction);
     } catch (error) {
       console.error(error);
-
-      if (!interaction.replied) {
-        try {
-          await interaction.reply({
-            content: "There was an error while executing this command!",
-            ephemeral: true,
-          });
-        } catch (e2) {
-          console.error(e2);
-        }
-        return;
-      }
-
-      const channel =
-        client.channels &&
-        typeof client.channels.get === "function" &&
-        client.channels.get(interaction.channelId);
-
-      if (channel) {
-        await channel.send({
-          content: "There was an error while executing this command!",
-          ephemeral: true,
-        });
-        return;
-      }
-
-      console.error("Unhandled error executing command!");
     }
   },
 };
